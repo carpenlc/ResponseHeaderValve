d41ec932302584d5d780c5ad93611c8e788a6dd2
Carpenter <carpenlc@ndwdsndwlpgrd01.arn.gov>
2019-09-09 14:00:07 +0000

    Updated to include 2 additional headers.

--- src/main/java/mil/nga/npd/ResponseHeaderValve/InsertResponseHeaders.java
+++ src/main/java/mil/nga/npd/ResponseHeaderValve/InsertResponseHeaders.java
@@ -13,11 +13,18 @@ import org.apache.catalina.valves.ValveBase;
  * through the web subsystem.  This class is an implementation of a simple 
  * global valve to insert response headers in the processing pipeline.  This 
  * implementation inserts the HTTP Strict-Transport-Security (HSTS) header 
- * as required by DoD STIGs.
+ * as required by DoD STIGs.  Updated to add headers to prevent MIME attacks
+ * and the IE8 XSS Protection headers.
  * 
  * @author L. Craig Carpenter
  */
 public class InsertResponseHeaders extends ValveBase {
+	
+	public static final String X_CONTENT_TYPE_HEADER_NAME = "X-Content-Type-Options";
+	public static final String X_CONTENT_TYPE_HEADER_VALUE = "nosniff";
+	
+	public static final String XSS_PROTECTION_HEADER_NAME = "X-XSS-Protection";
+	public static final String XSS_PROTECTION_HEADER_VALUE = "1; mode=block";
 	
 	public static final String HSTS_RESPONSE_HEADER_NAME = "Strict-Transport-Security";
 	public static final String HSTS_RESPONSE_HEADER_VALUE = "max-age=31536000; includeSubDomains; preload";
@@ -28,6 +35,12 @@ public class InsertResponseHeaders extends ValveBase {
 		// Add the HSTS header
 		resp.addHeader(HSTS_RESPONSE_HEADER_NAME, HSTS_RESPONSE_HEADER_VALUE);
 
+		// Add the X-Content-Type-Options header
+		resp.addHeader(X_CONTENT_TYPE_HEADER_NAME, X_CONTENT_TYPE_HEADER_VALUE);
+		
+		// Add the IE8 XSS protection header
+		resp.addHeader(XSS_PROTECTION_HEADER_NAME, XSS_PROTECTION_HEADER_VALUE);
+		
 		// Continue with the rest of the pipeline
 		getNext().invoke(req, resp);
 	}
